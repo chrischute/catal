da5e953a6387acc1be9accb8cf93ef37af9427ce
Christopher Chute <chutechristopher@gmail.com>
2018-08-08 09:04:56 -0700

    Update test script.

--- test.py
+++ test.py
@@ -1,11 +1,8 @@
-import torch
-import torch.nn.functional as F
-import util
-
 from args import TestArgParser
-from data_loader import FakeDataLoader
+from data_loader import WhiteboardLoader
+from logger import TestLogger
+from evaluator import ModelEvaluator
 from saver import ModelSaver
-from tqdm import tqdm
 
 
 def test(args):
@@ -15,20 +12,14 @@ def test(args):
     model = model.to(args.device)
     model.eval()
 
-    data_loader = FakeDataLoader(1000, args.num_classes, 'val', args.batch_size,
-                                 shuffle=False, do_augment=False, num_workers=args.num_workers)
-
-    # Get model outputs, log to TensorBoard, write masks to disk window-by-window
-    util.print_err('Writing model outputs to {}...'.format(args.results_dir))
-    with tqdm(total=len(data_loader.dataset), unit=' examples') as progress_bar:
-        for i, (inputs, info_dict) in enumerate(data_loader):
-            with torch.no_grad():
-                logits = model.forward(inputs.to(args.device))
-                probs = F.softmax(logits)
-
-            raise NotImplementedError('Test script in progress')
-
-            progress_bar.update(inputs.size(0))
+    # Run a single evaluation
+    eval_loader = WhiteboardLoader(args.data_dir, args.phase, args.batch_size,
+                                   shuffle=False, do_augment=False, num_workers=args.num_workers)
+    logger = TestLogger(args, len(eval_loader.dataset))
+    logger.start_epoch()
+    evaluator = ModelEvaluator(args.task_type, [eval_loader], logger, args.num_visuals)
+    metrics = evaluator.evaluate(model, args.device, logger.epoch)
+    logger.end_epoch(metrics)
 
 
 if __name__ == '__main__':
