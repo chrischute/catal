063c0c2d9fe21b36b5ce432c7fdd310500c13d92
Christopher Chute <chutechristopher@gmail.com>
2018-08-12 16:09:33 -0700

    Update download script to use pickle.

--- scripts/download_photos.py
+++ scripts/download_photos.py
@@ -2,6 +2,7 @@
 import argparse
 import os
 import pandas as pd
+import pickle
 import urllib.request
 
 from catal import CatalPhoto
@@ -12,20 +13,28 @@ IMAGENET_SIZE = 224, 224
 
 
 def main(args):
-    df = pd.read_csv(args.csv_path)
-    examples = [CatalPhoto(url=str(row[0]), annotation=str(row[1])) for _, row in df.iterrows()]
+    if args.pkl_path:
+        with open(args.pkl_path, 'rb') as pkl_fh:
+            examples = pickle.load(pkl_fh)
+        os.makedirs(os.path.join(args.output_dir, 'unlabeled'), exist_ok=True)
+    else:
+        df = pd.read_csv(args.csv_path)
+        examples = [CatalPhoto(url=str(row[0]), annotation=str(row[1])) for _, row in df.iterrows()]
 
-    print('Positives: {}'.format(sum(1 for e in examples if e.has_whiteboard is True)))
-    print('Negatives: {}'.format(sum(1 for e in examples if e.has_whiteboard is False)))
+        print('Positives: {}'.format(sum(1 for e in examples if e.has_whiteboard is True)))
+        print('Negatives: {}'.format(sum(1 for e in examples if e.has_whiteboard is False)))
 
-    # Make directories for holding photos
-    for dir_name in ('wb_pos', 'wb_neg'):
-        os.makedirs(os.path.join(args.output_dir, dir_name), exist_ok=True)
+        # Make directories for holding photos
+        for dir_name in ('wb_pos', 'wb_neg'):
+            os.makedirs(os.path.join(args.output_dir, dir_name), exist_ok=True)
 
     # Download photos
     for example in tqdm(examples):
         try:
-            subdir_name = 'wb_{}'.format('pos' if example.has_whiteboard else 'neg')
+            if example.is_labeled:
+                subdir_name = 'wb_{}'.format('pos' if example.has_whiteboard else 'neg')
+            else:
+                subdir_name = 'unlabeled'
             file_name = '{}.jpg'.format(example.photo_num)
             dst_path = os.path.join(args.output_dir, subdir_name, file_name)
             urllib.request.urlretrieve(example.url, dst_path)
@@ -44,6 +53,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Download photos from CSV file of URLs')
 
     parser.add_argument('--csv_path', default='data/wb_500sample.csv')
+    parser.add_argument('--pkl_path', default='/data/catal/wb_14k.pkl')
     parser.add_argument('--output_dir', default='/data/catal/wb500')
 
     main(parser.parse_args())
