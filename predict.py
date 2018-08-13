2ae26bcf27827d0f66923ee83edfd626ea0f3d6f
Christopher Chute <chutechristopher@gmail.com>
2018-08-12 23:20:39 -0700

    Results dir.

--- predict.py
+++ predict.py
@@ -36,7 +36,7 @@ def predict(args):
     record_ids = [os.path.basename(p)[:-4] for p in all_paths]  # Convert to record_id
     predictions = [int(p > args.prob_threshold) for p in all_probs]
     df = pd.DataFrame([{'record_id': r, 'has_whiteboard': p} for r, p in zip(record_ids, predictions)])
-    df.to_csv(os.path.join(args.save_dir, 'outputs.csv'))
+    df.to_csv(os.path.join(args.results_dir, 'outputs.csv'), index=False)
 
 
 if __name__ == '__main__':
