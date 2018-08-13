0a000371b920f31fdb70cd8c5aab96275582b8cc
Christopher Chute <chutechristopher@gmail.com>
2018-08-12 23:11:41 -0700

    Predict script.

---
+++ predict.py
@@ -0,0 +1,46 @@
+import optim
+import os
+import pandas as pd
+import torch
+
+from args import TestArgParser
+from data_loader import WhiteboardLoader
+from saver import ModelSaver
+
+
+def train(args):
+    model, ckpt_info = ModelSaver.load_model(args.ckpt_path, args.gpu_ids)
+    args.start_epoch = ckpt_info['epoch'] + 1
+    model = model.to(args.device)
+    model.train()
+
+    # Get optimizer and scheduler
+    parameters = optim.get_parameters(model.module, args)
+    optimizer = optim.get_optimizer(parameters, args)
+    lr_scheduler = optim.get_scheduler(optimizer, args)
+
+    # Get logger, evaluator, saver
+    data_loader = WhiteboardLoader(args.data_dir, args.phase, args.batch_size,
+                                   shuffle=True, do_augment=True, num_workers=args.num_workers)
+
+    # Predict outputs
+    all_probs, all_paths = [], []
+    for inputs, targets, paths in data_loader:
+
+        with torch.set_grad_enabled(True):
+            logits = model.forward(inputs.to(args.device))
+            probs = torch.sigmoid(logits)
+
+            all_probs += probs.to('cpu').numpy().tolist()
+            all_paths += list(paths)
+
+    # Write CSV
+    record_ids = [os.path.basename(p)[:-4] for p in all_paths]  # Convert to record_id
+    predictions = [int(p > args.prob_threshold) for p in all_probs]
+    df = pd.DataFrame([{'record_id': r, 'has_whiteboard': p} for r, p in zip(record_ids, predictions)])
+    df.to_csv(os.path.join(args.save_dir, 'outputs.csv'))
+
+
+if __name__ == '__main__':
+    parser = TestArgParser()
+    train(parser.parse_args())
