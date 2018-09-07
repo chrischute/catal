4ff6b2a06e8b513cc1bc6a74e9159e521a1e7ede
Christopher Chute <chutechristopher@gmail.com>
2018-09-07 10:44:07 -0700

    Update download_photos script.

--- scripts/download_photos.py
+++ scripts/download_photos.py
@@ -1,8 +1,14 @@
-"""Download photos using metadata from a CSV file."""
+"""Download photos using metadata from a CSV file.
+
+Usage:
+  1. Create CSV file with at least the column 'netpublish_URL'.
+     Note: 'original' gets replaced with 'preview'.
+  2. Make sure you've set up the catal environment, and run `source activate catal`.
+  3. Run `python download_photos.py --csv_path <PATH_TO_CSV> --output_dir <PATH_TO_OUTPUT_DIR>`
+"""
 import argparse
 import os
 import pandas as pd
-import pickle
 import requests
 import shutil
 import util
@@ -14,21 +20,13 @@ from requests.adapters import HTTPAdapter
 from requests.packages.urllib3.util.retry import Retry
 
 
-IMAGENET_SIZE = 224, 224
-
-
 def main(args):
-    if args.pkl_path:
-        with open(args.pkl_path, 'rb') as pkl_fh:
-            examples = pickle.load(pkl_fh)
-        os.makedirs(os.path.join(args.output_dir, 'unlabeled'), exist_ok=True)
-    else:
-        df = pd.read_csv(args.csv_path)
-        examples = [CatalPhoto(url=str(row['netpublish_URL']), annotation=None) for _, row in df.iterrows()]
+    df = pd.read_csv(args.csv_path)
+    examples = [CatalPhoto(url=str(row['netpublish_URL']), annotation=None) for _, row in df.iterrows()]
 
-        # Make directories for holding photos
-        for dir_name in ('wb_pos', 'wb_neg', 'unlabeled'):
-            os.makedirs(os.path.join(args.output_dir, dir_name), exist_ok=True)
+    # Make directories for holding photos
+    for dir_name in ('wb_pos', 'wb_neg', 'unlabeled'):
+        os.makedirs(os.path.join(args.output_dir, dir_name), exist_ok=True)
 
     session = requests.Session()
     retry = Retry(connect=3, backoff_factor=0.5)
@@ -37,7 +35,6 @@ def main(args):
     session.mount('https://', adapter)
 
     # Download photos
-    examples = examples[20200:]
     for example in tqdm(examples):
         if example.is_labeled:
             subdir_name = 'wb_{}'.format('pos' if example.has_whiteboard else 'neg')
@@ -61,16 +58,18 @@ def main(args):
             continue
 
         # Down-sample the image
-        img = Image.open(img_path, 'r').convert('RGB')
-        img = img.resize(IMAGENET_SIZE)
-        img.save(img_path)
+        if args.resize_shape is not None:
+            img = Image.open(img_path, 'r').convert('RGB')
+            img = img.resize(args.resize_shape)
+            img.save(img_path)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Download photos from CSV file of URLs')
 
     parser.add_argument('--csv_path', default='data/wb130k.csv')
-    parser.add_argument('--pkl_path', default='')
     parser.add_argument('--output_dir', default='data/wb130k')
+    parser.add_argument('--resize_shape', default=None, type=eval,
+                        help='Size to reshape downloaded images. E.g. "(224, 224)".')
 
     main(parser.parse_args())
